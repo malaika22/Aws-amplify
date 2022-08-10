06181743f926be24f9ad88e8a922205ecf6297bc
Malaika Afridi <malaikaafridi22@gmail.com>
2022-08-10 12:10:24 -0700

    feat: added login page

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { ChangeEventHandler, useState } from "react";
+import FormLayout from "../components/FormLayout";
+import TextField from "../components/TextField";
+
+const Home: NextPage = () => {
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+  });
+  const formChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+  return (
+    <div>
+      <FormLayout>
+        <h1 className='text-center text-2xl font-bold'>Login Form</h1>
+        <div>
+          <TextField
+            title='Email'
+            type='email'
+            name='email'
+            placeholder='Enter email'
+            onChange={formChange}
+          />
+          <TextField
+            title='Password'
+            type='password'
+            name='password'
+            placeholder='Enter password'
+            onChange={formChange}
+            toggleVisibility
+          />
+        </div>
+      </FormLayout>
+    </div>
+  );
+};
+
+export default Home;
